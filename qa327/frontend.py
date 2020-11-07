6c26ea81d1614fd04c09a7e79c4dc6acba2d90f2
Daniel Oh <37553054+Dan-Ial@users.noreply.github.com>
2020-11-07 10:22:16 -0500

    moved authenticate() in frontend.py to the top of the file and made it work for multiple functions; made /login redirect you to / if user is already logged in

--- qa327/frontend.py
+++ qa327/frontend.py
@@ -10,6 +10,42 @@ http requests from the client (browser) through templating.
 The html templates are stored in the 'templates' folder. 
 """
 
+def authenticate(inner_function):
+    """
+    :param inner_function: any python function that accepts a user object
+
+    Wrap any python function and check the current session to see if
+    the user has logged in. If login, it will call the inner_function
+    with the logged in user object.
+
+    To wrap a function, we can put a decoration on that function.
+    Example:
+
+    @authenticate
+    def home_page(user):
+        pass
+    """
+
+    def wrapped_inner():
+
+        # check did we store the key in the session
+        if 'logged_in' in session:
+            email = session['logged_in']
+            user = bn.get_user(email)
+            if user:
+                # if the user exists, call the inner_function
+                # with user as parameter
+                return inner_function(user)
+        else:
+            # else, redirect to the login page
+            return redirect('/login')
+
+    # Renaming the function name:
+    wrapped_inner.__name__ = inner_function.__name__
+
+    # return the wrapped version of the inner_function:
+    return wrapped_inner
+
 
 @app.route('/register', methods=['GET'])
 def register_get():
@@ -49,7 +85,10 @@ def register_post():
 
 
 @app.route('/login', methods=['GET'])
-def login_get():
+@authenticate
+def login_get(user):
+    if user:
+        return redirect('/')
     return render_template('login.html', message='Please login')
 
 
@@ -106,7 +145,7 @@ def authenticate(inner_function):
     """
     :param inner_function: any python function that accepts a user object
 
-    Wrap any python function and check the current session to see if 
+    Wrap any python function and check the current session to see if
     the user has logged in. If login, it will call the inner_function
     with the logged in user object.
 
