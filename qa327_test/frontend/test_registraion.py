54397dde8b80c44564ee10d063d7ff9d7703ba49
Ken West <kenneth.west@queensu.ca>
2020-11-20 17:20:41 -0500

    Completed test cases for R7 and R8

--- qa327_test/frontend/test_registraion.py
+++ qa327_test/frontend/test_registraion.py
@@ -24,7 +24,7 @@ Annotate @patch before unit tests can mock backend methods (for that testing fun
 # Mock a sample user
 test_user = User(
     email='test_frontend@test.com',
-    name='test_frontend',
+    name='Test_frontend$',
     password=generate_password_hash('test_frontend')
 )
 
@@ -82,3 +82,33 @@ class FrontEndHomePageTest(BaseCase):
         # make sure it shows proper error message
         self.assert_element("#message")
         self.assert_text("login failed", "#message")
+
+    #R7.1 : Logout will invalid the current session and redirect to the login page. After logout, the user shouldn't be able to access restricted pages.
+    def test_logout_redirect_to_login(self, *_):
+    	self.open(base_url + '/logout')
+    	self.open(base_url + '/login')
+    	self.type("#email", "test_frontend@test.com")
+    	self.type("#password", "Test_frontend$")
+    	self.click('input[type="submit"]')
+    	self.open(base_url)
+    	self.open(base_url + '/logout')
+    	self.open(base_url)
+    	self.assert_element("#message")
+    	self.assert_text("Please login", "#message")
+
+    #R8.1 : For any other requests except /login, /register, /, /login, /buy, /sell, the system should return a 404 error
+    def test_other_requests_are_404_errors(self, *_):
+    	self.open(base_url + '/logout')
+    	self.assert_no_404_errors()
+    	self.open(base_url)
+    	self.assert_no_404_errors()
+    	self.open(base_url + '/login')
+    	self.assert_no_404_errors()
+    	self.open(base_url + '/register')
+    	self.assert_no_404_errors()
+    	self.open(base_url + '/fake_domain')
+    	self.assert_element("#message")
+    	self.assert_text("Uh Oh! Something is not quite right here, maybe you tried to access a page you do not have access to or one that has recently been deleted.", "#message")
+
+
+
