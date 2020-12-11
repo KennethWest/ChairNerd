329a3c74bf335832bee3a31154c16cd1b7614400
Daniel Oh <37553054+Dan-Ial@users.noreply.github.com>
2020-12-11 16:13:24 -0500

    Started implementing integration test cases

--- qa327_test/integration/test_registration.py
+++ qa327_test/integration/test_registration.py
@@ -3,9 +3,13 @@ from seleniumbase import BaseCase
 
 from qa327_test.conftest import base_url
 import qa327.backend as bn
+from unittest.mock import patch
+from qa327.models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+import random
 
 
-# integration testing: the test case interacts with the 
+# integration testing: the test case interacts with the
 # browser, and test the whole system (frontend+backend).
 
 @pytest.mark.usefixtures('server')
@@ -36,17 +40,43 @@ class Registered(BaseCase):
         self.assert_text("Hi test0", "#welcome-header")
     '''
 
-    # def test_blackbox_get_user(self, *_):
-    #     """
-    #     Blackbox test case for backend function to get user
-    #     This test case registers a user, and then checks to see if they exist.
-    #     Then afterwards tests to see if an email which does not exist is also retrieved
-    #     (which it should not)
-    #     """
-    #     bn.register_user("test_frontendeee@test.com", "Tester", "TestFront#")
-    #     user = bn.get_user("test_frontendeee@test.com")
-    #     if user:
-    #         assert True
-    #     user_bad = bn.get_user("not_test_frontende@test.com")
-    #     if not user_bad:
-    #         assert True
+    def test_blackbox_get_user(self, *_):
+        """
+        Blackbox test case for backend function to get user
+        This test case registers a user, and then checks to see if they exist.
+        Then afterwards tests to see if an email which does not exist is also retrieved
+        (which it should not)
+        """
+        try:
+            bn.register_user("test_frontendeee@test.com", "Tester", "TestFront#")
+        except:
+            pass
+        user = bn.get_user("test_frontendeee@test.com")
+        if user:
+            assert True
+        user_bad = bn.get_user("not_test_frontende@test.com")
+        if not user_bad:
+            assert True
+
+    test_user_integration = User(
+        email='test_frontend@test.com',
+        name='test frontend',
+        password=generate_password_hash('Testfrontend#'),
+        balance=5000
+    )
+
+    @patch('qa327.backend.get_user', return_value=test_user_integration)
+    def test_integration_1(self, *_):
+        """
+        Integration test for creating postings
+        """
+        # make sure we're logged out
+        self.open(base_url + '/logout')
+        # open /login
+        self.open(base_url + '/login')
+        # type in login info
+        self.type('#email', 'test_frontend@test.com')
+        self.type('#password', 'Testfrontend#')
+        # click the log in button
+        self.click('input[type="submit"]')
+        # enter in the sell info
